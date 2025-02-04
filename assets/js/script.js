8ec5f801ddc8a82f3ab434cf58dae0d83666ac0e
arshad0812 <adarshadaashish@gmail.com>
2025-02-04 12:51:19 +0530

    commit on 4-2-2025

--- assets/js/script.js
+++ assets/js/script.js
@@ -35,14 +35,19 @@ addEventOnElem(navlink,"click",closeNavbar);
 
 
 const header=document.querySelector("[data-header]");
+const backTopBtn = document.querySelector("[data-back-top-btn]");
 
 
 const activeElem=function(){
     if(window.scrollY>100){
         header.classList.add("active");
+        backTopBtn.classList.add("active");
+
     }
     else{
         header.classList.remove("active");
+        backTopBtn.classList.remove("active");
+
     }
 }
 
